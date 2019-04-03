f59c9c4f44abac2723fb503987fb6a428730c3e6
Derek Rush <drush81@gmail.com>
2019-04-03 02:00:56 -0700

    add all

--- src/bouncing.ts
+++ src/bouncing.ts
@@ -1,14 +1,8 @@
-function eachBounce()
-
-function bouncingBall(h, bounce, window) {
+export function bouncingBall(h:number, bounce:number, window:number) {
   let bounceCount = 1
-  let bounceHeight = (bounce*100)/100 * h)
-  if(((bounce*100)/h) > window){
-    bounceCount = bounceCount + 2
-    console.log("Inner",bounceCount)
+  let bounceHeight = (bounce*100)/100 * h
+  for(bounceCount; bounceHeight > window; bounceCount = bounceCount + 2){
+    bounceHeight = (bounce*100)/100 * bounceHeight
   }
-  console.log("Outer",((bounce*100)/100 * h))
   return bounceCount
 }
-
-module.exports = ({ bouncingBall })
